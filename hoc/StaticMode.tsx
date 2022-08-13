45d29366c3a8006867a694b6c041e0f9555e920a
Benchapon <benchapons@skyller.co>
2022-08-13 22:47:14 +0700

    version 3

--- hoc/StaticMode.tsx
+++ hoc/StaticMode.tsx
@@ -7,17 +7,13 @@ const StaticMode = ({ children }: StaticModePropsType) => {
   const onHandleStaticMode = () => {
     if (typeof window !== "undefined") {
       let COLOR_THEME = localStorage?.getItem(NEXT_PUBLIC_THEME);
-      console.log(document.body.parentElement);
       if (COLOR_THEME) {
-        console.log("if");
-
         let htmlDataSet = document.body.parentElement?.dataset;
         if (htmlDataSet) {
           htmlDataSet.theme = COLOR_THEME;
           localStorage.setItem(NEXT_PUBLIC_THEME, htmlDataSet.theme);
         }
       } else {
-        console.log("else");
         localStorage.setItem(NEXT_PUBLIC_THEME, NEXT_PUBLIC_THEME_VALIE);
       }
     }
